62e0c9edbff9aed9a5db2bd60e0906f48ba30557
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2019-10-24 12:30:04 -0300

    Create ViewLogin

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Login.css'
+
+function Login() {
+return (
+    <div className="Login">
+      <header className="Login-header">
+      </header>
+      </div>
+  );
+}
+export default Login;
